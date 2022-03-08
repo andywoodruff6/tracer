63771895d7688aba6cfae510a6ee52f420f45224
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2022-03-07 20:59:38 -0600

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Home } from "./Pages/Home";
 import { SideBar } from "./Components/SideBar";
+import { Footer } from "./Components/Footer";
 export const App = () => {
   return (
-    <div className="flex justify-center">
-      <SideBar />
-      <Home />
-    </div>
+    <>
+      <div className="flex justify-center">
+        <SideBar />
+        <Home />
+      </div>
+      <Footer />
+    </>
   );
 };
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center pb-4 sm:p-4">
+      <div className="flex justify-center rounded-2xl bg-white py-5 px-7 sm:px-2">
+        <h5 className="font-mono text-lg font-semibold sm:text-2xl">
+          Built by:
+        </h5>
+        <h5 className="pl-1 font-mono text-xl font-semibold sm:text-2xl">
+          {" "}
+          THENEWBADGER.crypto
+        </h5>
+      </div>
+    </div>
+  );
 };
