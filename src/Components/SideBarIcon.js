b6609664e124a1c9c8c4c5eb9f86528c7c97adee
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2022-11-29 18:33:02 -0600

    convert project to Vite and Typescript

--- src/Components/SideBarIcon.js
+++
@@ -1,6 +0,0 @@
-export const SideBarIcon = ({ icon }) => (
-  <div className="sidebar-icon group">
-    {icon}
-    <span className="sidebar-tooltip group-hover:scale-100">{"Home"}</span>
-  </div>
-);
