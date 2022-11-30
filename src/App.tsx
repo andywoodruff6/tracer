b908f326fa98d13513ebffc5e6cb52c7860e4ccd
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2022-11-30 17:29:45 -0600

    adjusting css location

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Cardano from "./pages/cardano";
-import "./App.css";
+import "./style/App.css";
 
 function App() {
   const [count, setCount] = useState(0);
