4e77bcb16ccc41bd4971aa8b6a26eb2f9e8e1bc8
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2022-12-14 14:42:38 -0600

    clear commit

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-function Footer() {
-  return <div>
-    <p className="footerForNow">This is the footer.</p>
-  </div>;
-}
-
-export default Footer;
--- src/components/header.tsx
+++
@@ -1,8 +0,0 @@
-function Header() {
-  return <div>
-    <h1>Staking Tracer</h1>
-    <p className="navbarSoon">Insert Cool network selector here.</p>
-  </div>;
-  
-}
-export default Header;
